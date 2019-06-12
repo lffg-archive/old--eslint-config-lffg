aea75428d8dea07526fa1dd8c149376b59b8dc01
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-06-12 20:25:47 -0300

    lint: apply new lint rules

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-;(() => {
-  const pkg = require('../package.json')
-  console.log(`${pkg.name} → ${pkg.version}`)
-})()
+(() => {
+  const pkg = require('../package.json');
+  console.log(`${pkg.name} → ${pkg.version}`);
+})();
